d5d8c54cd5cc1020242eea2de6ab7abf59050598
mrutt <mrutt@hosting.com>
2019-06-28 15:08:03 -0400

    Add project files.

---
+++ SpreadsheetProcessor/Library.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetProcessor
+{
+    public static class Library
+    {
+        public static void WriteLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\Logfile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + " " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch ( Exception ex)
+            {
+
+            }
+        }
+
+    }
+}
---
+++ SpreadsheetProcessor/Scheduler.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Configuration;
+using GLAReportMVC;
+
+
+namespace SpreadsheetProcessor
+{
+    public partial class Scheduler : ServiceBase
+    {
+        private Timer timer = null;
+        public Scheduler()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer = new Timer(); 
+            //this.timer.Interval = 3600000; //hourly
+            this.timer.Interval = 500;
+            this.timer.Elapsed += new System.Timers.ElapsedEventHandler(this.timer_Tick);
+            timer.Enabled = true;
+            Library.WriteLog("Spreadsheet processor service started");
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void timer_Tick(object sender, ElapsedEventArgs e)
+        {
+            //Service action
+            var fileP = ConfigurationManager.AppSettings["SpreadsheetFolderOriginPath"];
+            var filePath = @"C:\Users\mrutt\Documents\GLASpreadsheets";
+            string[] files = Directory.GetFiles(filePath);
+
+            foreach (var fi in files)
+            {
+                //Perform the operations based on file type               
+                switch(fi)
+                {
+                    case var fl when (fi.Contains("MIN")):
+                        ProcessMIN(fi);
+                        break;
+                    case var fl when (fi.Contains("MAX")):
+                        ProcessMAX(fi);
+                        break;
+                    case var fl when (fi.Contains("Avg")):
+                        ProcessAvg(fi);
+                        break;
+                }
+            }
+        }
+
+        private void ProcessMIN(string fileName)
+        {
+            //Execute the code for the MIN spreadsheet
+            var usage = new DataSources.CMDB();
+            try
+            {
+                usage.ConvertUsageSheetToList(fileName, null, null);
+            }
+            catch (Exception ex)
+            {
+                Library.WriteLog(ex.ToString());
+            }
+        }
+
+
+        private void ProcessMAX(string fileName)
+        {
+            //Execute the code for the MAX readsheet
+            var usage = new DataSources.CMDB();
+            try
+            {
+                usage.ConvertUsageSheetToList(null, fileName, null);
+            }
+            catch (Exception ex)
+            {
+                Library.WriteLog(ex.InnerException.ToString());
+            }
+        }
+
+        private void ProcessAvg(string fileName)
+        {
+            //Execute the code for the MAX readsheet
+            var usage = new DataSources.CMDB();
+            try
+            {
+                usage.ConvertUsageSheetToList(null, null, fileName);
+            }
+            catch (Exception ex)
+            {
+                Library.WriteLog(ex.ToString());
+            }
+        }
+
+    }
+}
